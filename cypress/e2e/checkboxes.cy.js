aad3c8a462040a853a44dd3466b9a5b6fdbb9c11
its-yuvi-bhalla <yuvrajbhalla09gmail.com>
2024-03-13 00:11:20 +0530

    checkboxes

---
+++ cypress/e2e/checkboxes.cy.js
@@ -0,0 +1,23 @@
+describe("check boxes",() =>{
+    it("checkboxes",() =>{
+    
+    cy.visit("https://the-internet.herokuapp.com/")
+    
+    cy.get('[href="/checkboxes"]').click()
+    
+    cy.get("h3").should("not.contain","hi")
+    
+    cy.get('[checked=""]').click()
+
+    cy.get('#checkboxes > :nth-child(1)').click()
+    
+    
+    }
+    
+    )
+    
+    
+    
+    }
+    
+    )
