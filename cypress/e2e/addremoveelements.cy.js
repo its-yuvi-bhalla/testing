b466acd992999b3d0f05d86e87918299e6c8704f
its-yuvi-bhalla <yuvrajbhalla09gmail.com>
2024-03-12 21:32:52 +0530

    addremoveelements

---
+++ cypress/e2e/addremoveelements.cy.js
@@ -0,0 +1,23 @@
+describe("Add remove elements",()  =>{
+    it("Elements",() =>{
+
+        cy.visit("https://the-internet.herokuapp.com/")
+
+        cy.get('[href="/add_remove_elements/"]').click()
+
+        cy.get("h3").should("contain","Elements")
+
+        cy.get("button").should("contain","Add")
+
+        cy.get("button").click()
+
+        cy.get(".added-manually").click()
+
+
+
+    
+    
+    
+    
+    }) 
+})
