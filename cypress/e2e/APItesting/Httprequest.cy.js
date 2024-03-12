c57dba993bee35c33a20067761267e782481d80f
its-yuvi-bhalla <yuvrajbhalla09gmail.com>
2024-03-12 12:06:59 +0530

    done

---
+++ cypress/e2e/APItesting/Httprequest.cy.js
@@ -0,0 +1,36 @@
+describe("Httprequests",() => {
+    it("GET Call",() => {
+
+    cy.request("GET", "https://the-internet.herokuapp.com/")
+    .its("status")
+    .should("equal", 200);
+})
+
+it("POST Call", ()=>{
+
+    cy.request(  {
+               method: "POST",
+               url: "https://the-internet.herokuapp.com/",
+               body:  {
+                      title:"Test post",
+                      body:"This is post call",
+                      userId:1                 
+                      }
+                   })
+
+                   .its("status")
+                   .should("equal", 201);
+
+
+
+
+
+
+})
+
+
+
+
+
+
+})
